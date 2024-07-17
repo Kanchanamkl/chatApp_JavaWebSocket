7655c3fa0d1677dd259af3f97c2e0137c5ae36ae
Kanchanamkl <92777840+Kanchanamkl@users.noreply.github.com>
2024-07-17 10:52:15 +0530

    initial commit

---
+++ client_app/src/main/java/org/example/Client.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author : Kanchana Kalansooriya
+ * @since 7/17/2024
+ */
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Client 1!");
+    }
+}
---
+++ server_app/src/main/java/org/example/ChatServer.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author : Kanchana Kalansooriya
+ * @since 7/17/2024
+ */
+public class ChatServer {
+    public static void main(String[] args) {
+        System.out.println("Chat Server");
+    }
+}
